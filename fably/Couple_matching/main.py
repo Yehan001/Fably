1cdc0c6ad315ad5ab3d664d5673a7f94b0dd15db
Yehan001 <yehan.20232373@iit.ac.lk>
2025-04-07 19:03:20 +0530

    Update main.py

--- fably/Couple_matching/main.py
+++ fably/Couple_matching/main.py
@@ -88,3 +88,4 @@ print(f"Matching outfit for {gender_input}:", matching_outfit)
 gender_input = "child"
 matching_outfit = matcher.get_matching_outfit(gender_input, style_choice)
 print(f"Matching outfit for {gender_input}:", matching_outfit)
+
